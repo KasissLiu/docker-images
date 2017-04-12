952f04e55efb9648cae49b01f57bed8d174d0dab
KasissLiu <xueyezhishang@163.com>
2017-04-12 23:01:52 +0800

    add php-swoole
    add swoole.so , modified mysql datadir created and make reds open

--- lnmp/run.sh
+++ lnmp/run.sh
@@ -1,2 +1,7 @@
 #! /usr/bin/bash
-/usr/sbin/init & /usr/sbin/nginx & /usr/sbin/php-fpm & redis-server & /usr/bin/mysqld_safe --basedir=/usr
+DIRECTORY=/var/lib/mysql
+if [ "`ls -A $DIRECTORY`" = "" ]; then
+/usr/bin/mysql_install_db --user=mysql
+fi
+
+/usr/sbin/init & /usr/sbin/nginx & /usr/sbin/php-fpm & redis-server /etc/redis.conf & /usr/bin/mysqld_safe --basedir=/usr
